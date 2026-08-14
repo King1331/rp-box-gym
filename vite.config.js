1dccfd97d314bc918ff74f1a5254c970babb3a98
Rodeimy <maikelandres2134@gmail.com>
2026-08-14 15:22:41 -0600

    feat: Load images without wifi

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import path from "path"
 import { fileURLToPath } from "url"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa' // 1. Importamos el plugin
+import { VitePWA } from 'vite-plugin-pwa'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
@@ -10,12 +10,12 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 export default defineConfig({
   plugins: [
     react(),
-    // 2. Agregamos la configuración del Service Worker
     VitePWA({ 
       registerType: 'autoUpdate',
-      manifest: false, // <-- IMPORTANTE: Le dice a Vite que respete el manifest.json que ya tienes en public/
+      manifest: false, 
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'] // Hace que la app funcione offline guardando estos archivos
+        // Añadimos jpg, jpeg y webp para que la imagen del hero se guarde offline
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,webp}']
       }
     })
   ],
